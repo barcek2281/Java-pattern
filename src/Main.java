7b79b9e62347d9b3e04b140ab5ddd333d1b8d493
barcek2281 <sabdp17@gmail.com>
2024-09-29 20:48:19 +0500

    nothing

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import SOLID.dependency_inversion.*;
 
 public class Main {
     public static void main(String[] args) {
-        Button button = new Button(new Light());
-        button.press();
-        button.release();
+
     }
 }
---
+++ src/SOLID/single_responsibility/testing.java
@@ -0,0 +1,4 @@
+package SOLID.single_responsibility;
+
+public class testing {
+}
